c9bf75ee7e51cb561815024d84166214a218b5eb
Sistemas Operativos <so@sistemas-operativos.ibero.mx>
2021-04-12 17:02:48 +0100

    Entrega 1 (Falta info de particiones)

--- Codigo/ProyectoFinal.c
+++ Codigo/ProyectoFinal.c
@@ -7,6 +7,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <curses.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <math.h>
 
 #define MBR_SIZE 512 //particiones 512 bytes
 #define numPartitions 4 //cuatro particiones
@@ -20,56 +27,101 @@ static uint8_t mbr[512]; //El sector debe ser de ese tamaño
 
 void valoresCHS(uint32_t);
 void valorSector(uint32_t);
+void verParticion(unsigned int c,unsigned int h,unsigned int s,unsigned int sector);
+void cursesInit();
 
 int main(int argc, char **argv){
+	int x=0, y=1, j=0, z=0, Sam=20;
+	unsigned int c, h, s, sector;
+	int Caracter=0;
+	cursesInit();
+		if (argc < 2) {
+			printw("usage: %s (filename)\n", argv[0]);
+			exit(0);
+		}
 
-	system("clear");
-	if (argc < 2) {
-		printf("usage: %s (filename)\n", argv[0]);
-		exit(0);
-	}
+		// open & read primer particion en mbr 
+		FILE *file = fopen(argv[1], "r");
+		if (!file) {
+			printw("No se pudo abrir %s.\n", argv[1]);
+			exit(0);
+		}
+		fread(&mbr, MBR_SIZE, 1, file);
 
-	// open & read primer particion en mbr 
-	FILE *file = fopen(argv[1], "r");
-	if (!file) {
-		printf("No se pudo abrir %s.\n", argv[1]);
-		exit(0);
-	}
+		
+	do{
+		clear();
+		//Checar que tenga MBR ROOT signature
+		if (mbr[MBRend + 1] == 0xaa && mbr[MBRend] == 0x55)
+			printw("MBR");
+		else {
+			printw("No hay MBR ROOT signature");
+			exit(0);
+		}
 
-	fread(&mbr, MBR_SIZE, 1, file);
+		//Ya imprimimos
+		printw("\t\tC\tH\tS\tSector\n");
 
+		//Leyendo las particiones una por una
+		for (uint8_t i = 0; i < numPartitions; i++) 
+		{
+			uint32_t Partition = MBRstart + i * 0x10;
+			valoresCHS(Partition + 0x1);
+			valorSector(Partition + 0x8);
+		}
+		move(y,x);
+		refresh();
+		Caracter = getch();
+		switch(Caracter){
+			char temp[300];
+			case KEY_DOWN:
+				if(y<4){
+					y++;
+				}else{
+					y=1;
+				}
+				break;
+			case KEY_UP:
+				if(y>1){
+					y--;
+				}else{
+					y=4;
+				}
+				break;
+			case 10:
+				winstr(stdscr, temp);
+				sscanf(temp, "\t\t%u \t%u \t%u\t%u \n", &c, &h, &s, &sector);
+				verParticion(c, h, s, sector);
+
+			break;
+		}
+		clear();
+		refresh();
+	}while(Caracter!='q');
 	
-	//Checar que tenga MBR ROOT signature
-	if (mbr[MBRend + 1] == 0xaa && mbr[MBRend] == 0x55)
-		puts("MBR");
-	else {
-		puts("No hay MBR ROOT signature");
-		exit(0);
-	}
-
-	//Ya imprimimos
-	puts("\t\tC\tH\tS\tSector");
-
-	//Leyendo las particiones una por una
-	for (uint8_t i = 0; i < numPartitions; i++) 
-	{
-		uint32_t Partition = MBRstart + i * 0x10;
-		valoresCHS(Partition + 0x1);
-		valorSector(Partition + 0x8);
-	}
-
-	printf("\n\nPresiona [enter] para regresar al menu.\n");
-    __fpurge(stdin);
-    getchar();
+	 //End window.
+    endwin();
 }
 
 //Valores CHS
 void valoresCHS(uint32_t Partition) {
-	uint8_t c, h, s;
-	c = mbr[Partition];
-	h = mbr[Partition + 0x1];
-	s = mbr[Partition + 0x2];
-	printf("\t\t%u \t%u \t%u", c, h, s);
+	char s_bin[20];
+	uint8_t h, s, so;
+	uint16_t c;
+	h = mbr[Partition];
+	so = mbr[Partition + 0x1];
+	/*sprintf(s_bin, "%b", s);
+	move(5, 0);
+	printw("%s\n", s_bin);*/
+	s = so&0x3f;
+	c = mbr[Partition + 0x2];
+	// Opcion 1
+	c = c + (so&0xc0)*4;
+	// Opcion 2
+	
+	//c = c + so - s;
+	printw("\t\t%u \t%u \t%u", c, h, s);
+	//printw("    \t%u", so);
 }
 
 
@@ -79,11 +131,32 @@ void valorSector(uint32_t Partition) {
 	uint32_t sector = *(uint32_t *)inicio;
 	if(sector == 0)
 	{
-		puts("\tNo hay particion");
+		printw("\tNo hay particion\n");
 	}
 	else
 	{
-		printf("\t%u \n", sector);
+		printw("\t%u \n", sector);
 	}
 }
 
+void cursesInit(){
+	 //We initialize the curses screen
+    initscr();
+    keypad(stdscr, TRUE);
+    noecho();
+    cbreak();
+    refresh();
+}
+
+void verParticion(unsigned int c,unsigned int h,unsigned int s,unsigned int sector){
+	clear();
+
+
+
+
+
+
+
+	getch();
+	refresh();
+}
